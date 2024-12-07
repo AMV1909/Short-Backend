65a0e360d48e7e64e66476d91ad61fb471d7e496
AMV1909 <amorales264@unab.edu.co>
2024-12-07 12:35:01 -0500

    Updated allowed origins

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,6 +1,10 @@
 export const configCors = {
     credentials: true,
     allowedHeaders: `Content-Type,Authorization`,
-    origin: ["http://localhost:3000", ""],
+    origin: [
+        "http://localhost:3000",
+        "https://short-backend-production.up.railway.app/",
+        "https://short-frontend.pages.dev/",
+    ],
     optionsSuccessStatus: 200,
 };
