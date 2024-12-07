e083604c38fe3a631ca599b9395b1c3bf14d2f8a
AMV1909 <amorales264@unab.edu.co>
2024-12-07 12:52:41 -0500

    Deleted cors config

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 
-import { configCors } from "./config/cors";
 import { router } from "./routes";
 
 const app = express();
@@ -11,7 +10,7 @@ const app = express();
 app.set("port", process.env.PORT || 5000);
 
 // Middlewares
-app.use(cors(configCors));
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
