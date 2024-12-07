1f7e8a5be1be9e0dd2f923cd8f97d12d61ce80bb
AMV1909 <amorales264@unab.edu.co>
2024-12-07 12:59:37 -0500

    Updated cors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 
+import { configCors } from "./config/cors";
 import { router } from "./routes";
 
 const app = express();
@@ -10,7 +11,7 @@ const app = express();
 app.set("port", process.env.PORT || 5000);
 
 // Middlewares
-app.use(cors());
+app.use(cors(configCors));
 app.use(helmet());
 app.use(express.json());
 
